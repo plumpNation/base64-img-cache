8c0dc11bb28fa38a78facd760e97644a813b13a6
Gavin King <gavin.king@fronter.com>
2014-06-14 15:24:43 +0200

    Added test for medium images and a menu

--- main.js
+++ main.js
@@ -1,15 +1,78 @@
 (function () {
     'use strict';
 
-    var users = [
-            {id: 'ousaf32o313', image: 'images/1.jpg'},
-            {id: 'sf83jf0fahs', image: 'images/2.jpg'},
-            {id: '83720hfsf7s', image: 'images/3.jpg'},
-            {id: 'hfas87f3h32', image: 'images/4.png'},
-            {id: 'asf373h0138', image: 'images/5.jpg'}
-        ],
+    // I had to use locally hosted images as there are issues with extracting base64 easily
+    // using canvas when the image is cross domain.
+    var getSizeParam = function () {
+            var params = document.URL.split('?')[1];
 
-        testSize = function (callback) {
+            if (!params) {
+                return 'small';
+            }
+
+            switch (true) {
+            case params.indexOf('medium') > -1:
+                return 'medium';
+
+            default:
+                return 'small';
+            }
+        },
+
+
+        testSize = getSizeParam() || 'medium', // size of images, relates to folder name in images folder.
+        idLength = 20, // how long we want the id to be
+        nameLength = 20, // how long we want the username to be
+        fileType = 'jpg',
+
+        numOfTestImages = {
+            'small': 5,
+            'medium': 20
+        },
+
+        /**
+         * Creates a random string of alphanumeric characters.
+         *
+         * @param  {number} length How long we want the string to be.
+         * @return {string}
+         */
+        makeId = function (length) {
+            var text = '',
+                possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+            length = length || 5;
+
+            for (var i = 0; i < length; i += 1) {
+                text += possible.charAt(Math.floor(Math.random() * possible.length));
+            }
+
+            return text;
+        },
+
+        createUsers = function (imageSize) {
+            var result = [],
+                i;
+
+            for (i = 1; i <= numOfTestImages[testSize]; i += 1) {
+                result.push({
+                    'id': makeId(idLength),
+                    'name': makeId(nameLength),
+                    'image': 'images/' + testSize + '/' + i + '.' + fileType
+                })
+            }
+
+            return result; // this will be the value of users
+        },
+
+        users = createUsers(testSize),
+
+        /**
+         * This function will fill your storage up to it's limit.
+         *
+         * @param  {Function} callback Runs BEFORE you clear the test data out. Synchronous.
+         * @return {void}
+         */
+        maxOutStorage = function (callback) {
             var i,
                 data;
 
@@ -34,15 +97,45 @@
             localStorage.removeItem('DATA');
         },
 
-        getStorageSize = function (n) {
-            // quick hack to make the numbers correct
-            n = n / users.length;
+        /**
+         * Prettifies bytes number to it's most useful unit.
+         *
+         * @param  {number} bytes
+         * @return {string}       e.g. 2MB
+         */
+        bytesToSize = function (bytes) {
+            var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'],
+                i;
 
-            return JSON.stringify(window.localStorage).length * (n || 1);
+            if (bytes === 0) {
+                return '0 Bytes';
+            }
+
+           i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)), 10);
+
+           return Math.round(bytes / Math.pow(1024, i), 2) + ' ' + sizes[i];
         },
 
-        getPrettyStorageSize = function (n) {
-            return bytesToSize(getStorageSize(n));
+        /**
+         * Simply a way to count the bytes in the localStorage.
+         *
+         * @param  {number} multiplier
+         * @return {number}
+         */
+        getStorageSize = function (multiplier) {
+            // Quick hack to make the numbers correct
+            multiplier = multiplier / users.length;
+
+            return JSON.stringify(window.localStorage).length * (multiplier || 1);
+        },
+
+        /**
+         * Runs the result of getStorageSize through a prettifier function called bytesToSize
+         * @param  {number} multiplier
+         * @return {number}
+         */
+        getPrettyStorageSize = function (multiplier) {
+            return bytesToSize(getStorageSize(multiplier));
         },
 
         /**
@@ -94,63 +187,52 @@
             window.localStorage.setItem(key, data);
         },
 
-        cacheImage = function (img) {
+        /**
+         * Converts image tag to base 64 and writes the result to the local storage.
+         *
+         * @param  {DOMelement} img
+         * @return {void}
+         */
+        cacheImage = function (img, user) {
             var dataURL = convertImgToBase64(img);
 
-            return {
-                forUser: function (user) {
-                    user.image = dataURL;
-                    writeDataToStorage(user.id, user);
-                }
-            };
+            user.image = dataURL;
+            writeDataToStorage(user.id, user);
         },
 
-        createImage = function (user, getFromCache) {
+        /**
+         * Creates the image tag for the image url to populate.
+         *
+         * @param  {object} user
+         * @return {promise}     Resolves to the img tag when it is loaded so that calculations
+         *                                can be done knowing that the image is loaded and
+         *                                stored.
+         */
+        createImage = function (user) {
             var img,
-                fromCache,
-                cachedUser = window.localStorage.getItem(user.id),
                 promise;
 
-            if (getFromCache && cachedUser) {
-                fromCache = JSON.parse(cachedUser).image;
-
-            } else if (getFromCache) {
-                console.info('Image not yet saved to storage');
-                return Promise.resolve(null);
-            }
-
             img = new Image();
-            img.src = getFromCache ? fromCache : user.image;
+            img.src = user.image;
 
             return new Promise(function (resolve) {
-                if (fromCache) {
+                img.onload = function () {
+                    cacheImage(img, user);
+                    // If we resolve in the onload function, the users will be added according
+                    // to when they load from their respective sources. So expect the users
+                    // to change order on the page.
                     resolve(img);
-
-                } else {
-                    img.onload = function () {
-                        cacheImage(img).forUser(user);
-                        // If we resolve in the onload function, the users will be added according
-                        // to when they load from their respective sources. So expect the users
-                        // to change order on the page.
-                        resolve(img);
-                    };
-                }
+                };
             });
         },
 
-        bytesToSize = function (bytes) {
-            var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'],
-                i;
-
-            if (bytes === 0) {
-                return '0 Bytes';
-            }
-
-           i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)), 10);
-
-           return Math.round(bytes / Math.pow(1024, i), 2) + ' ' + sizes[i];
-        },
-
+        /**
+         * Just for reporting purposes. bigNumbers is used as a way to create projections.
+         * Converts a simple object
+         *
+         * @param  {object} data
+         * @return {[type]}      [description]
+         */
         makeBarChartData = function (data) {
             var bigNumbers = [100, 500, 1000, 5000, 10000],
                 datasetData = Object.keys(data).map(function (key) {
@@ -174,10 +256,26 @@
                 ctx = chart.getContext('2d');
 
             new Chart(ctx).Bar(makeBarChartData(data));
+
+            return true;
         },
 
-        writeReport = function (data) {
-            var report = $('#report'),
+        /**
+         * Populates a table with data and projection sizes.
+         *
+         * @return {void}
+         */
+        writeReport = function () {
+            var data = {
+                    'Number of users': users.length,
+                    'Storage size'   : getPrettyStorageSize(),
+                    'For 100 users'  : getPrettyStorageSize(100),
+                    'For 200 users'  : getPrettyStorageSize(200),
+                    'For 500 users'  : getPrettyStorageSize(500),
+                    'For 1000 users' : getPrettyStorageSize(1000),
+                    'For 10000 users': getPrettyStorageSize(10000)
+                },
+                report = $('#report'),
                 template = report.find('.row-template').remove(),
                 clone;
 
@@ -189,50 +287,30 @@
             });
         },
 
-        report = function () {
-            var reportObj = {
-                'Number of users': users.length,
-                'Storage size'   : getPrettyStorageSize(),
-                'For 100 users'  : getPrettyStorageSize(100),
-                'For 200 users'  : getPrettyStorageSize(200),
-                'For 500 users'  : getPrettyStorageSize(500),
-                'For 1000 users' : getPrettyStorageSize(1000),
-                'For 10000 users': getPrettyStorageSize(10000)
-            };
-
+        recordEndResults = function () {
             console.info('So after writing our ' + users.length + ' user images we ' +
                 'have used roughly ' + getPrettyStorageSize() + ' bytes');
             console.log('This means for 10000 users we will use roughly ' + getPrettyStorageSize(10000));
 
             chartData.end = getStorageSize();
-            chartData.prediction = getPrettyStorageSize(10000);
 
-            drawChart(chartData);
-            writeReport(reportObj);
+            return chartData;
         },
 
         webImageContainer = document.getElementById('web-image-container'),
         storageImageContainer = document.getElementById('storage-image-container');
 
+    // We start by clearing the local storage for this domain.
     localStorage.clear();
+
     console.log('Starting storage size: ', getPrettyStorageSize());
 
     var writesToStorage = [],
         chartData = {
             // Record the starting storage size
             'start': getStorageSize(),
-            'end': undefined,
-            'prediction': undefined
+            'end': undefined
         };
-
-    // This foreach is only interesting if we have commented the localStorage.clear() line.
-    /*users.forEach(function (user) {
-        createImage(user, true).then(function (img) {
-            if (img) {
-                storageImageContainer.appendChild(img);
-            }
-        });
-    });*/
 
     users.forEach(function (user) {
         var storing = new Promise(function (resolve) {
@@ -245,5 +323,8 @@
         writesToStorage.push(storing);
     });
 
-    Promise.all(writesToStorage).then(report);
+    Promise.all(writesToStorage)
+        .then(recordEndResults)
+        .then(drawChart)
+        .then(writeReport);
 }());
